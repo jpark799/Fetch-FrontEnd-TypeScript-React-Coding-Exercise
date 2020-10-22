e49140ecf09f559380e18503340f5b3066f64f1d
jpark799 <jason.j.park79@gmail.com>
2020-10-22 00:26:27 -0400

    fetching data correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	useEffect(() => {
+    const enableCrossOriginRequests = "https://cors-anywhere.herokuapp.com/"
+    const requestUrl = 'https://fetch-hiring.s3.amazonaws.com/hiring.json'
+		fetch(enableCrossOriginRequests+requestUrl)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			})
+	}, []);
+	return (
+	<div className="App">
+
+	</div>
+	);
 }
-
-export default App;

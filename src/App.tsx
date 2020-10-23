0508047bd59a5e43ed49d133c1487ef320618c32
jpark799 <jason.j.park79@gmail.com>
2020-10-22 21:36:09 -0400

    built types, initial state, and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
+	interface IItems {
+		id: number;
+		listId: number;
+		name: string;
+	}
+
+	const initialState: IItems[] = [
+		{
+			id: 0,
+			listId: 0,
+			name: ""
+		}
+	]
+
+	const [items, setItems] = useState(initialState);
+	const [loadComplete, setLoadComplete] = useState(false);
+
 	useEffect(() => {
     const enableCrossOriginRequests = "https://cors-anywhere.herokuapp.com/"
     const requestUrl = 'https://fetch-hiring.s3.amazonaws.com/hiring.json'
@@ -9,6 +26,7 @@ export default function App() {
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
+				setLoadComplete(true);
 			})
 	}, []);
 	return (
